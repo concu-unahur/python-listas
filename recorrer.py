188111bf16105dd3665beef00be1bd14cae1995d
sebasped <sebasped@disroot.org>
2020-02-21 10:51:18 -0300

    ejemplito recorrer una lista de dos en dos

---
+++ recorrer.py
@@ -0,0 +1,21 @@
+def primera_letra(palabra):
+    return palabra[0]
+
+def concatena_letras(lista):
+    concat = ' '
+    for i in range(len(lista)):
+        concat = concat + lista[i]
+    return concat
+
+lista = ['casa', 'auto', 'pelo', 'broche', 'juguete', 'mate']
+
+while lista != []:
+    # armo la lista de las dos palabras a usar
+    letras = map(primera_letra, lista[:2])
+    letras_lista = list(letras)
+    # concateno las primeras letras de cada palabra
+    letras_concat = concatena_letras(letras_lista)
+    # imprimo el resultado
+    print(letras_concat)
+    # saco de la lista a las palabras que acabo de concatenar, para no volverlas a usar
+    lista[:2]=[]
